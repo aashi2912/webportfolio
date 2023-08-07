1c23ba540536789566a3c2998b0b7b4138170015
aashi2912 <thakkaraashi3@gmail.com>
2023-08-06 23:24:03 -0400

    resume link update

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
           ))}
           <li className="border w-fit border-designColor border-2 rounded-md px-4 py-2">
             <a
-              href="https://drive.google.com/file/d/1DOlBGqhe4TAg_ycSkSfO1n3vDZLtEESJ/view?usp=share_link"
+              href="https://drive.google.com/file/d/15t8uKljoI7rXWM2JpgJKKbcLtnWVaHAQ/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               s
@@ -81,7 +81,7 @@ const Navbar = () => {
                 ))}
                 <li className="border w-fit border-designColor border-2 rounded-md px-4 py-2">
                   <a
-                    href="https://drive.google.com/file/d/1DOlBGqhe4TAg_ycSkSfO1n3vDZLtEESJ/view?usp=share_link"
+                    href="https://drive.google.com/file/d/15t8uKljoI7rXWM2JpgJKKbcLtnWVaHAQ/view?usp=drive_link"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
